9ca7c5ad363dc8b20565d303af0d60983402e641
Edgar Messia <ynfantes@gmail.com>
2021-07-28 14:57:11 +0200

    Elimina datos sensibles

--- includes/configuracion.php
+++ includes/configuracion.php
@@ -5,9 +5,9 @@ $email_error = TRUE;
 $mostrar_error = TRUE;
 $produccion = FALSE;
 if ($_SERVER['SERVER_NAME'] == "www.administradora-octagon.com.ve" | $_SERVER['SERVER_NAME'] == "administradora-octagon.com.ve") {
-    $user = "octagon_root";
-    $password = "Octagon5231";
-    $db = "valoriza2_octagon";
+    $user = "";
+    $password = "";
+    $db = "";
     $email_error = TRUE;
     $mostrar_error = FALSE;
     $debug = FALSE;
