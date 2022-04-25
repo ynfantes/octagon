687a0a78687aedeeed8caff07b296ef0717d7377
Edgar Messia <ynfantes@gmail.com>
2022-04-25 12:45:13 +0200

    FEAT se agrega el API de sincronización web

---
+++ api/src/routes/facturas.php
@@ -0,0 +1,69 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+include_once '../../includes/constants.php';
+
+
+
+$app->post('/facturas/insert', function(Request $req, Response $res) {
+    try {
+        
+        $factura = new factura();    
+        $data = json_decode($req->getBody(),true);
+    
+        foreach ($data as $index => $fact) {
+            unset($fact['id'],$fact['fidea']);
+            $result = $factura->insertarActualizar($fact);
+            $data[$index]['suceed'] = $result['suceed'];
+        }
+        $newRes = $res->withJson($data);
+        return $newRes;
+
+    } catch (\Throwable $th) {
+        return anError($th, $res);
+    }
+    
+});
+
+$app->delete('/facturas/delete', function(Request $req, Response $res) {
+    try {
+        
+        $factura = new factura();
+        $data = json_decode($req->getBody(),true);
+        
+        foreach ($data as $index => $fact) {
+            unset($fact['id']);
+            $result = $factura->borrarFactura($fact);
+            $data[$index]['suceed'] = $result['suceed'];
+        }
+        $newRes = $res->whithJson($data);
+        return $newRes;
+
+    } catch (\Throwable $th) {
+        return anError($th, $res);
+    }
+
+});
+
+$app->post('/avisos/strToPDF', function(Request $req, Response $res) {
+
+    try {
+        
+        $data = json_decode($req->getBody(),true);
+        //Decode pdf content
+        $pdf_decoded = base64_decode($data['base64']);
+        //Write data back to pdf file
+        $pdf = fopen('../../enlinea/avisos/'.$data['filename'],'w');
+        $data['suceed'] = fwrite($pdf,$pdf_decoded);
+        unset($data['base64']);
+        //close output file
+        fclose($pdf);
+        $newRes = $res->whitJson($data);
+        return $newRes;
+
+    } catch (\Throwable $th) {
+        return anError($th, $res);
+    }
+
+});
---
+++ api/src/routes/inmuebles.php
@@ -0,0 +1,170 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+include_once '../../includes/constants.php';
+
+$app->post('/inmuebles/insert', function(Request $req, Response $res) {
+    try {
+        
+        $inmueble = new inmueble();    
+        $data = json_decode($req->getBody(),true);
+        foreach ($data as $index => $inm) {
+            $result = $inmueble->insertarActualizar($inm);
+            $data[$index]['suceed'] = $result['suceed'];
+            $data[$index]['stats']  = $result['stats'];
+        }
+        $newRes = $res->withJson($data);
+        return $newRes;
+
+    } catch (\Throwable $th) {
+        return anError($th, $res);
+    }
+});
+
+
+$app->put('/cuentas', function(Request $req, Response $res) {
+    try {
+        
+        $inmueble = new inmueble();
+        $data = json_decode($req->getBody(),true);
+        $act = $data;
+        unset($act['IDCuenta'],$act['id']);
+        $result = $inmueble->actualizar($data['id'],$act);
+        $data['suceed'] = $result['suceed'];
+        $data['stats'] = $result['stats'];
+        $newRes = $res->withJson($data);
+        return $newRes;
+
+    } catch (\Throwable $th) {
+        return anError($th, $res);
+    }
+});
+
+
+$app->post('/cuentas', function(Request $req, Response $res) {
+    try {
+        
+        $inmueble = new inmueble();
+        $data = json_decode($req->getBody(),true);
+        foreach ($data as $index => $cuenta) {
+            unset($cuenta['IDCuenta']);
+            $result = $inmueble->agregarCuentaInmueble($cuenta);
+            $data[$index]['suceed']  = $result['suceed'];
+            $data[$index]['stats']   = $result['stats'];
+        }
+        $newRes = $res->withJson($data);
+        return $newRes;
+
+    } catch (\Throwable $th) {
+        return anError($th, $res);
+    }
+});
+
+
+$app->delete('/cuentas', function(Request $req, Response $res) {
+    try {
+        
+        $inmueble = new inmueble();
+        $data = json_decode($req->getBody(),true);
+        foreach ($data as $index => $cuenta) {
+            $result = $inmueble->borrarCuentaBancaria($cuenta['id_inmueble'],$cuenta['numero_cuenta']);
+            $data[$index]['suceed'] = $result['suceed'];
+            $data[$index]['stats']  = $result['stats'];
+        }
+        $newRes = $res->withJson($data);
+        return $newRes;
+
+    } catch (\Throwable $th) {
+        return anError($th, $res);
+    }
+});
+
+$app->put('/cobranza/mensual', function(Request $req, Response $res) {
+    try {
+        
+        $inmueble = new inmueble();
+        $data = json_decode($req->getBody(),true);
+        foreach ($data as $index => $cobranza) {
+            $result = $inmueble->insertarCobranzaMensual($cobranza);
+            $data[$index]['suceed'] = $result['suceed'];
+            $data[$index]['stats']   = $result['stats'];
+        }
+        $newRes = $res->withJson($data);
+        return $newRes;
+
+    } catch (\Throwable $th) {
+        return anError($th, $res);
+    }
+});
+
+$app->put('/facturacion/mensual', function(Request $req, Response $res) {
+    try {
+        
+        $inmueble = new inmueble();
+        $data = json_decode($req->getBody(),true);
+        foreach ($data as $index => $facturacion) {
+            $result = $inmueble->insertarFacturacionMensual($facturacion);
+            $data[$index]['suceed'] = $result['suceed'];
+            $data[$index]['stats']   = $result['stats'];
+        }
+        $newRes = $res->withJson($data);
+        return $newRes;
+
+    } catch (\Throwable $th) {
+        return anError($th, $res);
+    }
+});
+
+$app->put('/propietarios/grupo', function(Request $req, Response $res) {
+    try {
+        
+        $inmueble = new inmueble();
+        $result = json_decode($req->getBody(),true);
+        $codinm = '';
+        foreach ($result as $item => $prop) {
+    
+            if ($codinm !== $prop['id_inmueble']) {
+                $codinm = $prop['id_inmueble'];
+                $condicion = array_filter($prop, function($key) {
+                    return $key === 'id_inmueble';
+                },ARRAY_FILTER_USE_KEY);
+                $r = $inmueble->borrarGrupo($condicion);
+            }
+    
+            $r = $inmueble->insertarGrupo($prop);
+            $result[$item]['suceed'] = $r['suceed'];
+            $result[$item]['stats'] = $r['stats'];
+            if ($r['stats']['error']) $result[$item]['query'] = $r['query'];
+            
+        }
+        $newRes = $res->withJson($result);
+        return $newRes;
+
+    } catch (\Throwable $th) {
+        return anError($th, $res);
+    }
+
+});
+
+$app->post('/propietarios/grupo', function(Request $req, Response $res) {
+    try {
+        $inmueble = new inmueble();
+        $result = json_decode($req->getBody(),true);
+        $codinm = '';
+        $grupo  = '';
+        foreach ($result as $item => $prop) {
+    
+            $r = $inmueble->insertarGrupoPropietario($prop);
+            $result[$item]['suceed'] = $r['suceed'];
+            $result[$item]['stats'] = $r['stats'];
+            if ($r['stats']['error']) $result[$item]['query'] = $r['query'];
+            
+        }
+        $newRes = $res->withJson($result);
+        return $newRes;
+    } catch (\Throwable $th) {
+        return anError($th, $res);        
+    }
+
+});
