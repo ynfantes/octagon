6cbb9f3519c73e013509eb4c44cc4428aa12ccdf
Edgar Messia <ynfantes@gmail.com>
2022-08-26 08:06:26 +0200

    FIX replace metrodo __autoupload Twig

--- includes/constants.php
+++ includes/constants.php
@@ -59,10 +59,6 @@ define("GRAFICO_COBRANZA",1);
 define("DEMO",0);
 define("MOVIMIENTO_FONDO",1);
 
-//https://www.google.com/settings/u/1/security/lesssecureapps
-// https://accounts.google.com/DisplayUnlockCaptcha
-//https://security.google.com/settings/security/activity?hl=en&pli=1
-
 // twig
 include_once dirname(dirname(dirname(__FILE__))).'/framework/twig/lib/Twig/Autoloader.php';
 include_once SERVER_ROOT . 'includes/extensiones.php';
@@ -82,13 +78,9 @@ $twig->addExtension(new extensiones());
 $twig->addExtension(new Twig_Extension_Debug());
 
 //autoload
-
-function __autoload($clase) {
-    include_once SERVER_ROOT . "/includes/" . $clase . ".php";
-}
-
-spl_autoload_register("__autoload", false);
-
+spl_autoload_register( function($class) {
+    include_once SERVER_ROOT.'/includes/'.$class.'.php';
+});
 
 if (isset($_GET['logout']) && $_GET['logout'] == true) {
     $user_logout = new propietario();
